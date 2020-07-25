89ec979315808b952fa6ef0194a2d1a5b084edb3
jjhiggz <jonathan.higger@gmail.com>
2020-07-24 19:13:41 -0600

    updated

--- js/GABUZOMEU.js
+++ js/GABUZOMEU.js
@@ -1,4 +1,5 @@
 // I won this competition!!! so I figured that I would save my code
+// the challenge included minimizing the amount of code, thats why it looks hideous
 const N=parseInt(readline()).toString(4).split('');
 let v={0:'GA',1:'BU',2:'ZO',3:'MEU'}
 console.log(N.reduce((a,n)=>a+v[+n],''));
